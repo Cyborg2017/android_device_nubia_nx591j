17f6b8afbf4c55a09626d415bed4bc71f735b89a
Cyborg2017 <wangyang0987654@gmail.com>
2019-11-13 12:28:14 +0800

    nx591j: audio: Remove common keylayout files

--- device.mk
+++ device.mk
@@ -26,9 +26,7 @@ PRODUCT_COPY_FILES +=  \
 
 # Keylaouts
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/gpio-keys.kl::$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/synaptics_dsx.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/synaptics_dsx.kl \
-    $(LOCAL_PATH)/keylayout/nubia_synaptics_dsx.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/nubia_synaptics_dsx.kl \
     $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/synaptics_rmi4_i2c.kl
 
 # Inherit from nubia msm8976-common
